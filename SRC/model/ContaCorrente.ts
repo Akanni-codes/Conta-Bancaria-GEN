65361bbb1b2cacb3c84a74fe24e77c2f70974bda
Akanni Silva <akanni029@gmail.com>
2025-09-01 20:09:05 -0300

    adicionando classe ContaCorrente

---
+++ SRC/model/ContaCorrente.ts
@@ -0,0 +1,44 @@
+import { Conta } from "./conta";
+
+export class ContaCorrente extends Conta {
+  private _limite: number;
+
+  constructor(
+    numero: number,
+    agencia: number,
+    tipo: number,
+    titular: string,
+    saldo: number,
+    limite: number
+  ) {
+    super(numero, agencia, tipo, titular, saldo);
+    this._limite = limite;
+  }
+
+  public get limite(): number {
+    return this._limite;
+  }
+
+  public set limite(limite: number) {
+    this._limite = limite;
+  }
+
+  /**
+   * sacar
+   */
+  public sacar(valor: number): boolean {
+    if (this.saldo + this._limite < valor) {
+      console.log("\n Saldo Insuficiente!");
+      return false;
+    }
+    this.saldo -= valor;
+    return true;
+  }
+  /**
+   * visualizar
+   */
+  public visualizar(): void {
+    super.visualizar();
+    console.log("Limite: " + this._limite.toFixed(2));
+  }
+}
