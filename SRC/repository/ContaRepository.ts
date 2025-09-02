b5ab4cc3c8b48ef50157a2a0a854844684a3f8ad
Akanni Silva <akanni029@gmail.com>
2025-09-02 10:53:46 -0300

    formatando texto do codigo ContaRepository

--- SRC/repository/ContaRepository.ts
+++ SRC/repository/ContaRepository.ts
@@ -1,15 +1,19 @@
 import { Conta } from "../model/conta";
 
 export interface ContaRepository {
-    // CRUD da conta
-    procurarporNumero(numero:number): void;
-    listarTodas(): void;
-    cadastrar(conta:Conta): void
-    atualizar(conta:Conta): void;
-    deletar(numero: number): void;
+  // CRUD da conta
+  procurarporNumero(numero: number): void;
+  listarTodas(): void;
+  cadastrar(conta: Conta): void;
+  atualizar(conta: Conta): void;
+  deletar(numero: number): void;
 
-    // Método bancarios
-    sacar(number:number, valor:number): void;
-    depositar(numero: number, valor: number):void;
-    transferir(numeroOrigigem: number, numeroDestino: number, valor: number): void;
+  // Método bancarios
+  sacar(number: number, valor: number): void;
+  depositar(numero: number, valor: number): void;
+  transferir(
+    numeroOrigigem: number,
+    numeroDestino: number,
+    valor: number
+  ): void;
 }
